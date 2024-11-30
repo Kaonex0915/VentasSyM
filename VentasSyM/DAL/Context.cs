78a81e48fc2094507cdd603180e536b4eaacdb25
Marcos Duran <Marcos_Duran@ucne.edu.do>
2024-11-30 16:00:45 -0400

    Subiendo el contexto

--- VentasSyM/DAL/Context.cs
+++ VentasSyM/DAL/Context.cs
@@ -1,6 +1,15 @@
-﻿namespace VentasSyM.DAL
+﻿using Microsoft.EntityFrameworkCore;
+using VentasSyM.Models;
+
+namespace VentasSyM.DAL;
+
+public class Context:DbContext
 {
-    public class Context
-    {
-    }
+    public Context (DbContextOptions<Context> options) : base(options) { }
+
+    public DbSet<Productos> Productos { get; set; }
+    public DbSet<Compras> Compras { get; set; }
+    public DbSet<Ventas> Ventas { get; set; }
+    public DbSet<ComprasDetalle> ComprasDetalle { get; set; }
+    public DbSet<VentasDetalle> VentasDetalles { get; set; }
 }
